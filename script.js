15c15eb6edcab42d5250e66ef7677ce4fe14495b
AJITHKUMAR M <ajithkumar12204@gmail.com>
2025-07-30 11:38:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 const songs = {
   happy: [
-    { title: "Vaathi Coming - Master", video: "https://www.youtube.com/embed/UTI0X0UeE-A" },
-    { title: "Jimikki Kammal - Velipadinte Pusthakam", video: "https://www.youtube.com/embed/y3wiW1aUQ00" },
-    { title: "Aathichudi - TN 07 AL 4777", video: "https://www.youtube.com/embed/GH5VmIipuxc" }
+    { title: "Vaathi Coming - Master", video: "https://www.youtube.com/embed/UEl4pT1kGqY" },
+    { title: "Jimikki Kammal", video: "https://www.youtube.com/embed/TJOP5WqI6D8" },
+    { title: "Aathichudi - TN 07 AL 4777", video: "https://www.youtube.com/embed/lF8P5wrKZV0" }
   ],
   sad: [
-    { title: "Unna Nenachu - Psycho", video: "https://www.youtube.com/embed/pxCzxxjP-TM" },
-    { title: "Ninaithu Ninaithu - 7G Rainbow Colony", video: "https://www.youtube.com/embed/K3KnWh1cw9A" },
-    { title: "Oru Naalil - Pudhupettai", video: "https://www.youtube.com/embed/8f5cZ28wErk" }
+    { title: "Unna Nenachu - Psycho", video: "https://www.youtube.com/embed/C05-BYt3YuQ" },
+    { title: "Ninaithu Ninaithu - 7G Rainbow Colony", video: "https://www.youtube.com/embed/mtDKVg6vW94" },
+    { title: "Oru Naalil - Pudhupettai", video: "https://www.youtube.com/embed/NuZy6z2t49A" }
   ],
   love: [
-    { title: "Kadhal Kan Kattudhe", video: "https://youtu.be/DjgwkfFnakM?si=GGQa-y0LPMYoxK4D" },
-    { title: "Enna Solla Pogirai", video: "https://www.youtube.com/embed/Qk19z8dN3HU" },
-    { title: "Munbe Vaa - Sillunu Oru Kaadhal", video: "https://www.youtube.com/embed/J40A3iQ9z1Y" }
+    { title: "Kadhal Kan Kattudhe", video: "https://www.youtube.com/embed/s-4TP_gjH5Q" },
+    { title: "Enna Solla Pogirai", video: "https://www.youtube.com/embed/l5p_P86aJ2U" },
+    { title: "Munbe Vaa - Sillunu Oru Kadhal", video: "https://www.youtube.com/embed/f5gOtEk8G-Q" }
   ],
   party: [
-    { title: "Aaluma Doluma - Vedalam", video: "https://www.youtube.com/embed/yvAxVj9TFAw" },
-    { title: "Sodakku - Thaanaa Serndha Koottam", video: "https://www.youtube.com/embed/qvOTIpRylNw" },
-    { title: "Local Boys - Ethir Neechal", video: "https://www.youtube.com/embed/Tya3vY1fYwQ" }
+    { title: "Aaluma Doluma - Vedalam", video: "https://www.youtube.com/embed/HBPSQffVUbw" },
+    { title: "Sodakku - Thaanaa Serndha Koottam", video: "https://www.youtube.com/embed/6SOZtrLZ2Ts" },
+    { title: "Local Boys - Ethir Neechal", video: "https://www.youtube.com/embed/q9xQIbRwffg" }
   ],
   calm: [
-    { title: "Pachai Nirame - Alaipayuthey", video: "https://www.youtube.com/embed/YY9MjGFfqx4" },
-    { title: "Engeyum Eppothum", video: "https://www.youtube.com/embed/M4h18ZocPfA" },
-    { title: "Vennilave - Minsara Kanavu", video: "https://www.youtube.com/embed/jXDBPivT2T8" }
+    { title: "Pachai Nirame - Alaipayuthey", video: "https://www.youtube.com/embed/R_dP4P82fQE" },
+    { title: "Engeyum Eppodhum", video: "https://www.youtube.com/embed/7ALa9Uu1CyY" },
+    { title: "Vennilave - Minsara Kanavu", video: "https://www.youtube.com/embed/H5uYbnS-5ic" }
   ],
   angry: [
-    { title: "Thee Illai - Arima Nambi", video: "https://www.youtube.com/embed/WK3zrQ7A59c" },
-    { title: "Udhungada Sangu - VIP", video: "https://www.youtube.com/embed/NsHY5JqShRM" },
-    { title: "Yaar Ivan", video: "https://www.youtube.com/embed/madI9G1CtSg" }
+    { title: "Thee Illai - Arima Nambi", video: "https://www.youtube.com/embed/i38mWnmkZAw" },
+    { title: "Udhungada Sangu - VIP", video: "https://www.youtube.com/embed/LctbYWwMZPw" },
+    { title: "Yaar Ivan", video: "https://www.youtube.com/embed/fmyT5AP7nrs" }
   ],
   romantic: [
-    { title: "Anbil Avan - VTV", video: "https://www.youtube.com/embed/lEPVkmXrUAA" },
-    { title: "Thalli Pogathey", video: "https://www.youtube.com/embed/YpuaZ5N1XZg" },
-    { title: "Neeyum Naanum Anbe", video: "https://www.youtube.com/embed/MQ9PlR6ZjzU" }
+    { title: "Anbil Avan - Vinnaithaandi Varuvaayaa", video: "https://www.youtube.com/embed/LNtvAT1oI7k" },
+    { title: "Thalli Pogathey", video: "https://www.youtube.com/embed/Du5gTVbHb6A" },
+    { title: "Neeyum Naanum Anbe", video: "https://www.youtube.com/embed/cWhZB6TfIgg" }
   ],
   energetic: [
-    { title: "Don'u Don'u Don'u - Maari", video: "https://www.youtube.com/embed/A4MmvMwqY8M" },
-    { title: "Oodha Coloru Ribbon", video: "https://www.youtube.com/embed/v8GO3iHxHfo" },
-    { title: "Mersal Arasan - Mersal", video: "https://www.youtube.com/embed/NcV0Lh1V5T4" }
+    { title: "Don'u Don'u Don'u - Maari", video: "https://www.youtube.com/embed/o6ZpY8VwBnc" },
+    { title: "Oodha Coloru Ribbon", video: "https://www.youtube.com/embed/rp4k5zAIFxI" },
+    { title: "Mersal Arasan", video: "https://www.youtube.com/embed/f2I_tFfMZFI" }
   ]
 };
 
