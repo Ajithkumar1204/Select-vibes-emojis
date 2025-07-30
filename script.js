4de7ce493f6429bb9d60eaa23fa05b1cec4ea1f2
AJITHKUMAR M <ajithkumar12204@gmail.com>
2025-07-30 23:20:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,120 +2,120 @@ const songs = {
   happy: [
     {
       title: "Vaathi Coming",
-      video: "https://www.youtube.com/watch?v=UTI0X0UeE-A",
-      img: "https://img.youtube.com/vi/UTI0X0UeE-A/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=fRD_3vJagxk",
+      img: "https://i.ytimg.com/vi/fRD_3vJagxk/maxresdefault.jpg"
     },
     {
       title: "Jimikki Kammal",
-      video: "https://www.youtube.com/watch?v=y3wiW1aUQ00",
-      img: "https://img.youtube.com/vi/y3wiW1aUQ00/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=Np29xQZiCQk",
+      img: "https://i.ytimg.com/vi/Np29xQZiCQk/maxresdefault.jpg"
     },
     {
       title: "Chill Bro",
-      video: "https://www.youtube.com/watch?v=JpL3gkYJ1Kk",
-      img: "https://img.youtube.com/vi/JpL3gkYJ1Kk/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=afm4lcz31MA",
+      img: "https://i.ytimg.com/vi/afm4lcz31MA/maxresdefault.jpg"
     }
   ],
   sad: [
     {
       title: "Unna Nenachu",
-      video: "https://www.youtube.com/watch?v=pxCzxxjP-TM",
-      img: "https://img.youtube.com/vi/pxCzxxjP-TM/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?app=desktop&v=jFWsj_QT0G8&t=2m44s",
+      img: "https://i.ytimg.com/vi/tA-Y7sUddlk/maxresdefault.jpg"
     },
     {
       title: "Ennodu Nee Irundhaal",
-      video: "https://www.youtube.com/watch?v=7qYHscb1Vb4",
-      img: "https://img.youtube.com/vi/7qYHscb1Vb4/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=EhhiY11Z9-U",
+      img: "https://i.ytimg.com/vi/EhhiY11Z9-U/mqdefault.jpg"
     },
     {
       title: "Oru Naalil",
-      video: "https://www.youtube.com/watch?v=3RWrjGJuhUI",
-      img: "https://img.youtube.com/vi/3RWrjGJuhUI/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=sFoLmT_pcLQ",
+      img: "https://i.ytimg.com/vi/sFoLmT_pcLQ/maxresdefault.jpg"
     }
   ],
   love: [
     {
       title: "Kadhal Kan Kattudhe",
-      video: "https://www.youtube.com/watch?v=TblG93FZktc",
-      img: "https://img.youtube.com/vi/TblG93FZktc/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=uxgxTANVKvc&pp=0gcJCfwAo7VqN5tD",
+      img: "https://i.ytimg.com/vi/DjgwkfFnakM/maxresdefault.jpg"
     },
     {
       title: "Enna Solla Pogirai",
-      video: "https://www.youtube.com/watch?v=Oy6HKIbfFdw",
-      img: "https://img.youtube.com/vi/Oy6HKIbfFdw/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=5_eAAZ9CgHA",
+      img: "https://i.ytimg.com/vi/5_eAAZ9CgHA/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDS60PJWjk6tFryJ_ufLoJbnD98_g"
     },
     {
       title: "Anbil Avan",
-      video: "https://www.youtube.com/watch?v=qq_Xy6IopM4",
-      img: "https://img.youtube.com/vi/qq_Xy6IopM4/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=Bcp0Jp_MUYU",
+      img: "https://i.ytimg.com/vi/Bcp0Jp_MUYU/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBh_k2F8pUUmYqHz6om9-YR5zTQMw"
     }
   ],
   party: [
     {
       title: "Aaluma Doluma",
-      video: "https://www.youtube.com/watch?v=yvAxVj9TFAw",
-      img: "https://img.youtube.com/vi/yvAxVj9TFAw/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=_85oqHgjWLo",
+      img: "https://i.ytimg.com/vi/_85oqHgjWLo/maxresdefault.jpg"
     },
     {
       title: "Vaathi Raid",
-      video: "https://www.youtube.com/watch?v=zjvYOfn2B94",
-      img: "https://img.youtube.com/vi/zjvYOfn2B94/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=eHoIUNY-bG4",
+      img: "https://i.ytimg.com/vi/eHoIUNY-bG4/maxresdefault.jpg"
     },
     {
       title: "Rowdy Baby",
-      video: "https://www.youtube.com/watch?v=x6QZn9xiuOE",
-      img: "https://img.youtube.com/vi/x6QZn9xiuOE/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=x6Q7c9RyMzk",
+      img: "https://static.moviecrow.com/marquee/yuvans-rowdy-baby-song-from-maari-2-sets-big-records/155406_thumb_665.jpg"
     }
   ],
   calm: [
     {
       title: "Pachai Nirame",
-      video: "https://www.youtube.com/watch?v=YY9MjGFfqx4",
-      img: "https://img.youtube.com/vi/YY9MjGFfqx4/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=HiocgIOIyik",
+      img: "https://i.ytimg.com/vi/HiocgIOIyik/maxresdefault.jpg"
     },
     {
       title: "Munbe Vaa",
-      video: "https://www.youtube.com/watch?v=YuIX6eL6ebM",
-      img: "https://img.youtube.com/vi/YuIX6eL6ebM/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=wJ93eYgiP98",
+      img: "https://i.ytimg.com/vi/wJ93eYgiP98/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC2fwVfJDj1D0OsfKrLIDu5pwDFKg"
     },
     {
       title: "Vaseegara",
-      video: "https://www.youtube.com/watch?v=R-YmHeM69yc",
-      img: "https://img.youtube.com/vi/R-YmHeM69yc/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?app=desktop&v=-6qDON8pFNE",
+      img: "https://i.ytimg.com/vi/-6qDON8pFNE/maxresdefault.jpg"
     }
   ],
   angry: [
     {
       title: "Udhungada Sangu",
-      video: "https://www.youtube.com/watch?v=NsHY5JqShRM",
-      img: "https://img.youtube.com/vi/NsHY5JqShRM/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=RCXzH27eOIA",
+      img: "https://i.ytimg.com/vi/RCXzH27eOIA/sddefault.jpg"
     },
     {
       title: "Surviva",
-      video: "https://www.youtube.com/watch?v=XPwVq56lFao",
-      img: "https://img.youtube.com/vi/XPwVq56lFao/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?app=desktop&v=QNZiwr7nYMk",
+      img: "https://i.ytimg.com/vi/QNZiwr7nYMk/maxresdefault.jpg"
     },
     {
       title: "Danga Maari",
       video: "https://www.youtube.com/watch?v=fWLvItDLgvY",
-      img: "https://img.youtube.com/vi/fWLvItDLgvY/hqdefault.jpg"
+      img: "https://i.ytimg.com/vi/ELDx-0S5IQM/maxresdefault.jpg"
     }
   ],
   romantic: [
     {
       title: "Thalli Pogathey",
-      video: "https://www.youtube.com/watch?v=YpuaZ5N1XZg",
-      img: "https://img.youtube.com/vi/YpuaZ5N1XZg/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=PiL5UTTTrxk&pp=0gcJCfwAo7VqN5tD",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI2-OmdNfJ-0PBtWIRuTWaVODJGWEStHhtMg&s"
     },
     {
       title: "Kanave Kanave",
-      video: "https://www.youtube.com/watch?v=XzErQ9N4mLQ",
-      img: "https://img.youtube.com/vi/XzErQ9N4mLQ/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=MofM9B2NZpY&pp=0gcJCfwAo7VqN5tD",
+      img: "https://pbs.twimg.com/media/FuPK_QXaIAATVvc.jpg:large"
     },
     {
       title: "New York Nagaram",
-      video: "https://www.youtube.com/watch?v=XzErQ9N4mLQ",
-      img: "https://img.youtube.com/vi/XzErQ9N4mLQ/hqdefault.jpg"
+      video: "https://www.youtube.com/watch?v=OjU54VhRFbU&pp=0gcJCfwAo7VqN5tD",
+      img: "https://i.ytimg.com/vi/OjU54VhRFbU/maxresdefault.jpg"
     }
   ],
   energetic: [
@@ -127,7 +127,7 @@ const songs = {
     {
       title: "Chellamma",
       video: "https://www.youtube.com/watch?v=DV7nV9W7y-0",
-      img: "https://c.saavncdn.com/489/Chellamma-From-Doctor--Tamil-2020-20200716110528-500x500.jpg"
+      img: "https://i.ytimg.com/vi/nAWg-XCucXg/maxresdefault.jpg"
     },
      {
       title: "Sodaku Mela",
