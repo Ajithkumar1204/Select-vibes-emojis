957399ff2189dda7bb74dc5f059caf5752e2e793
AJITHKUMAR M <ajithkumar12204@gmail.com>
2025-07-30 12:19:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
 const songs = {
-  happy: [
-    { title: "Vaathi Coming - Master", video: "https://www.youtube.com/embed/UEl4pT1kGqY" },
-    { title: "Jimikki Kammal", video: "https://www.youtube.com/embed/TJOP5WqI6D8" },
-    { title: "Aathichudi - TN 07 AL 4777", video: "https://www.youtube.com/embed/lF8P5wrKZV0" }
-  ],
-  sad: [
-    { title: "Unna Nenachu - Psycho", video: "https://www.youtube.com/embed/C05-BYt3YuQ" },
-    { title: "Ninaithu Ninaithu - 7G Rainbow Colony", video: "https://www.youtube.com/embed/mtDKVg6vW94" },
-    { title: "Oru Naalil - Pudhupettai", video: "https://www.youtube.com/embed/NuZy6z2t49A" }
-  ],
-  love: [
-    { title: "Kadhal Kan Kattudhe", video: "https://www.youtube.com/embed/s-4TP_gjH5Q" },
-    { title: "Enna Solla Pogirai", video: "https://youtu.be/xZ92nnR1Pt8?si=8kIF5mydUdN-C1XF" },
-    { title: "Munbe Vaa - Sillunu Oru Kadhal", video: "https://www.youtube.com/embed/f5gOtEk8G-Q" }
-  ],
-  party: [
-    { title: "Aaluma Doluma - Vedalam", video: "https://www.youtube.com/embed/HBPSQffVUbw" },
-    { title: "Sodakku - Thaanaa Serndha Koottam", video: "https://www.youtube.com/embed/6SOZtrLZ2Ts" },
-    { title: "Local Boys - Ethir Neechal", video: "https://www.youtube.com/embed/q9xQIbRwffg" }
-  ],
   calm: [
-    { title: "Pachai Nirame - Alaipayuthey", video: "https://www.youtube.com/embed/R_dP4P82fQE" },
-    { title: "Engeyum Eppodhum", video: "https://www.youtube.com/embed/7ALa9Uu1CyY" },
-    { title: "Vennilave - Minsara Kanavu", video: "https://www.youtube.com/embed/H5uYbnS-5ic" }
-  ],
-  angry: [
-    { title: "Thee Illai - Arima Nambi", video: "https://www.youtube.com/embed/i38mWnmkZAw" },
-    { title: "Udhungada Sangu - VIP", video: "https://www.youtube.com/embed/LctbYWwMZPw" },
-    { title: "Yaar Ivan", video: "https://www.youtube.com/embed/fmyT5AP7nrs" }
-  ],
-  romantic: [
-    { title: "Anbil Avan - Vinnaithaandi Varuvaayaa", video: "https://www.youtube.com/embed/LNtvAT1oI7k" },
-    { title: "Thalli Pogathey", video: "https://www.youtube.com/embed/Du5gTVbHb6A" },
-    { title: "Neeyum Naanum Anbe", video: "https://www.youtube.com/embed/cWhZB6TfIgg" }
-  ],
-  energetic: [
-    { title: "Don'u Don'u Don'u - Maari", video: "https://www.youtube.com/embed/o6ZpY8VwBnc" },
-    { title: "Oodha Coloru Ribbon", video: "https://www.youtube.com/embed/rp4k5zAIFxI" },
-    { title: "Mersal Arasan", video: "https://www.youtube.com/embed/f2I_tFfMZFI" }
+    {
+      title: "Pachai Nirame – Alaipayuthey",
+      lyrics: `பச்சை நிரமே பச்சை நிரமே
+உன்னைக் காணும் போதே
+என்னை மறந்தேனே...
+
+காற்றின் இசையே, தேனின் மொழியே
+தீண்டும் விழியில் தீண்டாமல் பேசும்...
+
+மழையே கண்களில் நின்று சொல்கிறதே
+மறைஞ்சு பேசும் உன்னோட வார்த்தைகள்...`
+    },
+    {
+      title: "Engeyum Eppothum – Ninaithale Inikkum",
+      lyrics: `எங்கேயும் எப்போதும் சின்ன சின்ன ஆசைகள்...
+எந்தன் மனதுக்குள்ளே வாழும் சின்னவளே...
+
+பூக்கள் பூக்கும் நேரமிது...
+பாடும் கிளிகள் கூவுதே...
+அவளே என் காதலியே...`
+    },
+    {
+      title: "Vennilave – Minsara Kanavu",
+      lyrics: `வெண்ணிலவே வெண்ணிலவே
+வந்தை இங்கே என் நிலவே...
+
+கன்னி நிலா காதல் நிலா
+கண்ணில் கனவில் என் உயிரே...
+
+மௌன இசை கேட்கும் நேரம்
+மழைததும்பும் நேரமிது...`
+    }
   ]
+  // You can add other moods like happy, sad, etc. similarly
 };
 
 function typeWriter(element, text, i = 0) {
@@ -51,7 +46,6 @@ function typeWriter(element, text, i = 0) {
 function showSongs(mood) {
   const container = document.getElementById("cardSection");
   container.innerHTML = "";
-
   songs[mood].forEach(song => {
     const card = document.createElement("div");
     card.className = "card";
@@ -60,10 +54,10 @@ function showSongs(mood) {
     card.appendChild(title);
     typeWriter(title, song.title);
 
-    const iframe = document.createElement("iframe");
-    iframe.src = song.video;
-    iframe.allowFullscreen = true;
-    card.appendChild(iframe);
+    const lyricsDiv = document.createElement("div");
+    lyricsDiv.className = "lyrics-box";
+    lyricsDiv.innerText = song.lyrics;
+    card.appendChild(lyricsDiv);
 
     container.appendChild(card);
   });
