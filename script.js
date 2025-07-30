52e2603385e2a565fc07ad6c8fdd73ba1ab15c9e
AJITHKUMAR M <ajithkumar12204@gmail.com>
2025-07-30 16:47:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,182 +1,66 @@
 const songs = {
-  calm: [
+  happy: [
     {
-      title: "Kannana Kanne - Viswasam",
-      video: "https://www.youtube.com/watch?v=B4MaoPLeNQY",
-      thumbnail: "https://img.youtube.com/vi/B4MaoPLeNQY/hqdefault.jpg"
-    },
-    {
-      title: "Anbil Avan - Vinnaithaandi Varuvaayaa",
-      video: "https://www.youtube.com/watch?v=Hqt2wNyNnRE",
-      thumbnail: "https://img.youtube.com/vi/Hqt2wNyNnRE/hqdefault.jpg"
-    },
-    {
-      title: "Moongil Thottam - Kadal",
-      video: "https://www.youtube.com/watch?v=yZx3qfFq6cA",
-      thumbnail: "https://img.youtube.com/vi/yZx3qfFq6cA/hqdefault.jpg"
-    }
-  ],
-  love: [
-    {
-      title: "Ennodu Nee Irundhaal - I",
-      video: "https://www.youtube.com/watch?v=StVwC-EpE1s",
-      thumbnail: "https://img.youtube.com/vi/StVwC-EpE1s/hqdefault.jpg"
-    },
-    {
-      title: "Kadhal Cricket - Thani Oruvan",
-      video: "https://www.youtube.com/watch?v=RY5kL_T6ZfY",
-      thumbnail: "https://img.youtube.com/vi/RY5kL_T6ZfY/hqdefault.jpg"
-    },
-    {
-      title: "Enna Solla Pogirai - Kandukondain Kandukondain",
-      video: "https://www.youtube.com/watch?v=rOyKGS1uTTU",
-      thumbnail: "https://img.youtube.com/vi/rOyKGS1uTTU/hqdefault.jpg"
-    }
-  ],
-  motivational: [
-    {
-      title: "Unakkul Naane - Pachaikili Muthucharam",
-      video: "https://www.youtube.com/watch?v=Z5fWB7FVAbQ",
-      thumbnail: "https://img.youtube.com/vi/Z5fWB7FVAbQ/hqdefault.jpg"
-    },
-    {
-      title: "Aalaporan Thamizhan - Mersal",
-      video: "https://www.youtube.com/watch?v=OUq5mRKCQms",
-      thumbnail: "https://img.youtube.com/vi/OUq5mRKCQms/hqdefault.jpg"
-    },
-    {
-      title: "Rise of Dhanush - VIP",
-      video: "https://www.youtube.com/watch?v=dIMKzssPncg",
-      thumbnail: "https://img.youtube.com/vi/dIMKzssPncg/hqdefault.jpg"
-    }
-  ],
-  party: [
-    {
-      title: "Vaathi Coming - Master",
+      title: "Vaathi Coming",
       video: "https://www.youtube.com/watch?v=UTI0X0UeE-A",
-      thumbnail: "https://img.youtube.com/vi/UTI0X0UeE-A/hqdefault.jpg"
+      img: "https://img.youtube.com/vi/UTI0X0UeE-A/hqdefault.jpg"
     },
     {
-      title: "Local Boys - Ethir Neechal",
-      video: "https://www.youtube.com/watch?v=EZRVqSXoBl0",
-      thumbnail: "https://img.youtube.com/vi/EZRVqSXoBl0/hqdefault.jpg"
-    },
-    {
-      title: "Why This Kolaveri Di - 3",
-      video: "https://www.youtube.com/watch?v=YR12Z8f1Dh8",
-      thumbnail: "https://img.youtube.com/vi/YR12Z8f1Dh8/hqdefault.jpg"
+      title: "Jimikki Kammal",
+      video: "https://www.youtube.com/watch?v=y3wiW1aUQ00",
+      img: "https://img.youtube.com/vi/y3wiW1aUQ00/hqdefault.jpg"
     }
   ],
   sad: [
     {
-      title: "Unna Nenachu - Psycho",
+      title: "Unna Nenachu",
       video: "https://www.youtube.com/watch?v=pxCzxxjP-TM",
-      thumbnail: "https://img.youtube.com/vi/pxCzxxjP-TM/hqdefault.jpg"
-    },
-    {
-      title: "Ninaithu Ninaithu - 7G Rainbow Colony",
-      video: "https://www.youtube.com/watch?v=K3KnWh1cw9A",
-      thumbnail: "https://img.youtube.com/vi/K3KnWh1cw9A/hqdefault.jpg"
-    },
-    {
-      title: "Oru Naalil - Pudhupettai",
-      video: "https://www.youtube.com/watch?v=8f5cZ28wErk",
-      thumbnail: "https://img.youtube.com/vi/8f5cZ28wErk/hqdefault.jpg"
+      img: "https://img.youtube.com/vi/pxCzxxjP-TM/hqdefault.jpg"
     }
   ],
-  cool: [
+  love: [
     {
-      title: "Chill Bro - Pattas",
-      video: "https://www.youtube.com/watch?v=4GGo2zrD0jQ",
-      thumbnail: "https://img.youtube.com/vi/4GGo2zrD0jQ/hqdefault.jpg"
-    },
-    {
-      title: "Don'u Don'u Don'u - Maari",
-      video: "https://www.youtube.com/watch?v=Gp4aZfZhAqI",
-      thumbnail: "https://img.youtube.com/vi/Gp4aZfZhAqI/hqdefault.jpg"
-    },
-    {
-      title: "Kalyanam - Bangalore Days",
-      video: "https://www.youtube.com/watch?v=tjbYgLZr5rY",
-      thumbnail: "https://img.youtube.com/vi/tjbYgLZr5rY/hqdefault.jpg"
+      title: "Kadhal Kan Kattudhe",
+      video: "https://www.youtube.com/watch?v=TblG93FZktc",
+      img: "https://img.youtube.com/vi/TblG93FZktc/hqdefault.jpg"
     }
   ],
-  comedy: [
+  party: [
     {
-      title: "Ding Dong - Jil Jung Juk",
-      video: "https://www.youtube.com/watch?v=2ysMN8zE1mM",
-      thumbnail: "https://img.youtube.com/vi/2ysMN8zE1mM/hqdefault.jpg"
-    },
-    {
-      title: "Ulagam Oruvanukka - Kabali (Funny Remix)",
-      video: "https://www.youtube.com/watch?v=6xT4bMxZYyw",
-      thumbnail: "https://img.youtube.com/vi/6xT4bMxZYyw/hqdefault.jpg"
-    },
-    {
-      title: "Appadi Podu - Ghilli",
-      video: "https://www.youtube.com/watch?v=8AQKpvOQZxE",
-      thumbnail: "https://img.youtube.com/vi/8AQKpvOQZxE/hqdefault.jpg"
+      title: "Aaluma Doluma",
+      video: "https://www.youtube.com/watch?v=yvAxVj9TFAw",
+      img: "https://img.youtube.com/vi/yvAxVj9TFAw/hqdefault.jpg"
     }
   ],
-  romantic: [
+  calm: [
     {
-      title: "Vaseegara - Minnale",
-      video: "https://www.youtube.com/watch?v=iZ9w0AZe_fI",
-      thumbnail: "https://img.youtube.com/vi/iZ9w0AZe_fI/hqdefault.jpg"
-    },
-    {
-      title: "Munbe Vaa - Sillunu Oru Kadhal",
-      video: "https://www.youtube.com/watch?v=lzN6Txp9jms",
-      thumbnail: "https://img.youtube.com/vi/lzN6Txp9jms/hqdefault.jpg"
-    },
-    {
-      title: "Pookkal Pookkum - Thamizh",
-      video: "https://www.youtube.com/watch?v=Y9nLMTQ6O6M",
-      thumbnail: "https://img.youtube.com/vi/Y9nLMTQ6O6M/hqdefault.jpg"
+      title: "Pachai Nirame",
+      video: "https://www.youtube.com/watch?v=YY9MjGFfqx4",
+      img: "https://img.youtube.com/vi/YY9MjGFfqx4/hqdefault.jpg"
     }
   ],
   angry: [
     {
-      title: "Surviva - Vivegam",
-      video: "https://www.youtube.com/watch?v=ZKjZb8AO9Ds",
-      thumbnail: "https://img.youtube.com/vi/ZKjZb8AO9Ds/hqdefault.jpg"
-    },
-    {
-      title: "Thaarame Thaarame (BG Angry Edit)",
-      video: "https://www.youtube.com/watch?v=U6uTrw1-7Wc",
-      thumbnail: "https://img.youtube.com/vi/U6uTrw1-7Wc/hqdefault.jpg"
-    },
-    {
-      title: "Psycho Saiyaan - Saaho",
-      video: "https://www.youtube.com/watch?v=ptNaCg6XvCk",
-      thumbnail: "https://img.youtube.com/vi/ptNaCg6XvCk/hqdefault.jpg"
+      title: "Udhungada Sangu",
+      video: "https://www.youtube.com/watch?v=NsHY5JqShRM",
+      img: "https://img.youtube.com/vi/NsHY5JqShRM/hqdefault.jpg"
     }
   ],
-  peace: [
+  romantic: [
     {
-      title: "Nenjukkul Peidhidum - Vaaranam Aayiram",
-      video: "https://www.youtube.com/watch?v=a2QuxQYXNoA",
-      thumbnail: "https://img.youtube.com/vi/a2QuxQYXNoA/hqdefault.jpg"
-    },
+      title: "Thalli Pogathey",
+      video: "https://www.youtube.com/watch?v=YpuaZ5N1XZg",
+      img: "https://img.youtube.com/vi/YpuaZ5N1XZg/hqdefault.jpg"
+    }
+  ],
+  energetic: [
     {
-      title: "Suttrum Vizhi - Ghajini",
-      video: "https://www.youtube.com/watch?v=GfywW1fZsrY",
-      thumbnail: "https://img.youtube.com/vi/GfywW1fZsrY/hqdefault.jpg"
-    },
-    {
-      title: "Oru Deivam Thantha Poove - Kannathil Muthamittal",
-      video: "https://www.youtube.com/watch?v=QrNaVKkLqY8",
-      thumbnail: "https://img.youtube.com/vi/QrNaVKkLqY8/hqdefault.jpg"
+      title: "Don'u Don'u Don'u",
+      video: "https://www.youtube.com/watch?v=A4MmvMwqY8M",
+      img: "https://img.youtube.com/vi/A4MmvMwqY8M/hqdefault.jpg"
     }
   ]
 };
-
-function typeWriter(element, text, i = 0) {
-  if (i < text.length) {
-    element.innerHTML += text.charAt(i);
-    setTimeout(() => typeWriter(element, text, i + 1), 25);
-  }
-}
 
 function showSongs(mood) {
   const container = document.getElementById("cardSection");
@@ -185,17 +69,20 @@ function showSongs(mood) {
   songs[mood].forEach(song => {
     const card = document.createElement("div");
     card.className = "card";
-    card.onclick = () => window.open(song.video, "_blank");
 
     const img = document.createElement("img");
-    img.src = song.thumbnail;
+    img.src = song.img;
     img.alt = song.title;
-    img.className = "thumb";
-    card.appendChild(img);
 
     const title = document.createElement("h3");
+    title.textContent = song.title;
+
+    card.appendChild(img);
     card.appendChild(title);
-    typeWriter(title, song.title);
+
+    card.addEventListener("click", () => {
+      window.open(song.video, "_blank");
+    });
 
     container.appendChild(card);
   });
